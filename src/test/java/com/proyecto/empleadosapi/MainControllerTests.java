1601f76351eb0a19ef161e57c06cae5676ca569c
Emilio Santos <emsantos@hacienda.gov.do>
2023-10-10 14:38:31 -0400

    Añadiendo test funcionales al MainController

--- src/test/java/com/proyecto/empleadosapi/MainControllerTests.java
+++ src/test/java/com/proyecto/empleadosapi/MainControllerTests.java
@@ -6,21 +6,26 @@ import com.proyecto.empleadosapi.model.Employee;
 import com.proyecto.empleadosapi.repository.EmployeeRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
 import java.util.Date;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
+@ExtendWith(MockitoExtension.class)
 public class MainControllerTests {
 
     @Autowired
@@ -31,6 +36,10 @@ public class MainControllerTests {
 
     @Autowired
     private EmployeeRepository employeeRepository;
+
+    @Mock
+    private EmployeeRepository employeeRepositoryMocked;
+    private static ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void testVerificarEndPointRecuperarEmpleado() throws Exception {
@@ -44,8 +53,64 @@ public class MainControllerTests {
                 .andReturn();
 
         String json = result.getResponse().getContentAsString();
-        Employee fetchedEmployee = new ObjectMapper().readValue(json, Employee.class);
+        Employee fetchedEmployee = objectMapper.readValue(json, Employee.class);
 
         Assertions.assertTrue(fetchedEmployee.equals(employee));
+    }
+
+    @Test
+    public void testVerficarEndPointPostEmpleado() throws Exception {
+        Employee newEmployee = new Employee("Bryan", "Bocio", BigDecimal.valueOf(52000), new Date(), "Developer", "TI");
+
+        String requestJson = new ObjectMapper().writeValueAsString(newEmployee);
+
+        MvcResult result = mockMvc.perform(post("/employee/add")
+                        .header("X-API-KEY", "Baeldung")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                        .andExpect(status().isOk()).andReturn();
+
+        Assertions.assertEquals(result.getResponse().getContentAsString(), "Guardado correctamente");
+    }
+
+    @Test
+    public void testVerificarEndPointActualizaEmpleado() throws Exception {
+        Employee newEmployee = new Employee("Bryan", "Bocio", BigDecimal.valueOf(52000), new Date(), "Developer", "TI");
+
+        String requestJson = new ObjectMapper().writeValueAsString(newEmployee);
+
+        mockMvc.perform(put("/employee/1")
+                        .header("X-API-KEY", "Baeldung")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                        .andExpect(status().isOk()).andReturn();
+
+        MvcResult result =  this.mockMvc.perform(get("/employee/1")
+                        .header("X-API-KEY", "Baeldung"))
+                        .andExpect(status().isOk())
+                        .andReturn();
+
+        String json = result.getResponse().getContentAsString();
+        Employee fetchedEmployee = objectMapper.readValue(json, Employee.class);
+
+        Assertions.assertTrue(fetchedEmployee.equals(newEmployee));
+    }
+
+    @Test
+    public void testVerificarEndPointEliminaEmpleado() throws Exception {
+        Employee newEmployee = new Employee("Bryan", "Bocio", BigDecimal.valueOf(52000), new Date(), "Developer", "TI");
+
+        String requestJson = new ObjectMapper().writeValueAsString(newEmployee);
+
+        mockMvc.perform(delete("/employee/26")
+                        .header("X-API-KEY", "Baeldung")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                        .andExpect(status().isOk()).andReturn();
+
+        MvcResult result =  this.mockMvc.perform(get("/employee/1")
+                        .header("X-API-KEY", "Baeldung"))
+                        .andExpect(status().isNotFound())
+                        .andReturn();
     }
 }
